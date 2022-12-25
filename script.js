cadf0f465ca5990d88db44e0b5daf4877e090c9a
Duodoodle <79131923+Duodoodle@users.noreply.github.com>
2022-12-25 19:16:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,38 +7,3 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-60px";
   }
 }
-function active1() {
-    document.getElementById("home").classList.add("active");
-    document.getElementById("causes").classList.remove("active");
-    document.getElementById("effects").classList.remove("active");
-    document.getElementById("prevention").classList.remove("active");
-    document.getElementById("about").classList.remove("active");
-}
-function active2() {
-    document.getElementById("causes").classList.add("active");
-    document.getElementById("home").classList.remove("active");
-    document.getElementById("effects").classList.remove("active");
-    document.getElementById("prevention").classList.remove("active");
-    document.getElementById("about").classList.remove("active");
-}
-function active3() {
-    document.getElementById("effects").classList.add("active");
-    document.getElementById("causes").classList.remove("active");
-    document.getElementById("home").classList.remove("active");
-    document.getElementById("prevention").classList.remove("active");
-    document.getElementById("about").classList.remove("active");
-}
-function active4() {
-    document.getElementById("prevention").classList.add("active");
-    document.getElementById("causes").classList.remove("active");
-    document.getElementById("effects").classList.remove("active");
-    document.getElementById("home").classList.remove("active");
-    document.getElementById("about").classList.remove("active");
-}
-function active5() {
-    document.getElementById("about").classList.add("active");
-    document.getElementById("causes").classList.remove("active");
-    document.getElementById("effects").classList.remove("active");
-    document.getElementById("prevention").classList.remove("active");
-    document.getElementById("home").classList.remove("active");
-}
